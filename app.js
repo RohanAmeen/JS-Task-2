22eb8b3406e88a64641649e5720e27f30f011c18
RohanAmeen <rohanameen55@gmail.com>
2023-09-07 21:58:15 +0500

    JS Chap 5 Practice

---
+++ app.js
@@ -0,0 +1,133 @@
+//PROGRAM NO 1
+// var add = 3+5;
+// document.write("Sum oF 3 and 5 is "+add);
+
+//PROGRAM NO 2
+// var sub = 3-5;
+// document.write("Subraction of 3 and 5 is "+sub);
+
+// var mul = 3*5;
+// document.write("Multiplication of 3 and 5 is "+mul);
+
+// var div = 3/5;
+// document.write("Division of 3 and 5 is "+div);
+
+// var mod = 3%5;
+// document.write("Modulus of 3 and 5 is "+mod);
+
+//PROGRAM NO 3
+// var action;
+// document.write("Value after variable declaration is: "+action);
+
+// action=5;
+// document.write("<br>Initial Value: "+action);
+
+// ++action;
+// document.write("<br>Value after increment is: "+action);
+
+// action=action+7;
+// document.write("<br>Value after addition is: "+action);
+
+// --action;
+// document.write("<br>Value after decrement is:"+action);
+
+// action=action%3;
+// document.write("<br>The Remainder is: "+action);
+
+//PROGRAM NO 4
+// var ticketprice=600;
+// var fiveticket=ticketprice*5;
+// document.write("Total cost to buy 5 tickets to a movie is "+fiveticket+"PKR");
+
+//PROGRAM NO 5
+// var x=4;
+// document.write("Table Of "+x);
+// for(var i=1;i<=10;i++){
+//     document.write("<br>"+x+" x "+i+" = "+x*i);
+// }
+
+//PROGRAM NO 6
+// // Step a: Store a Celsius temperature into a variable.
+// var celsiusTemperature = 25; 
+
+// // Step b: Convert Celsius to Fahrenheit & output the result.
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// document.write(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F");
+
+// // Step c: Store a Fahrenheit temperature into a variable.
+// var fahrenheitTemperature2 = 70; 
+
+// // Step d: Convert Fahrenheit to Celsius & output the result.
+// var celsiusTemperature2 = (fahrenheitTemperature2 - 32) * 5/9;
+// document.write("<br>"+fahrenheitTemperature2 + "°F is " + celsiusTemperature2 + "°C");
+
+//PROGRAM NO 7
+// document.write("<b>Shopping Cart</b><br><br>");
+// var item1price = 650;
+// document.write("Price of item 1 is " +item1price+"<br>");
+// var item1quantity = 3;
+// document.write("Quantity of item 1 is " +item1quantity+"<br>");
+
+// var item2price = 100;
+// document.write("Price of item 2 is " +item2price+"<br>");
+// var item2quantity = 7;
+// document.write("Quantity of item 2 is " +item2quantity+"<br>");
+// var shippingCharges = 100;
+// document.write("Shipping charges "+shippingCharges+"<br><br>");
+
+// var totalcost = item1price * item1quantity + item2price * item2quantity + shippingCharges;
+// document.write("Total cost of your order is " + totalcost);
+
+//PROGRAM NO 8
+// document.write("<b>Marks Sheet</b><br><br>")
+// var totalMarks = 980;
+//  document.write("Total marks: " + totalMarks +"<br>");
+//  var obtainedMarks = 804;
+//  document.write("Marks obtained: " + obtainedMarks +"<br>");
+// var percentage = obtainedMarks/totalMarks*100;
+//  document.write("Percentage: " + percentage +"%");
+
+//PROGRAM NO 9
+// document.write("<b>Currency In PKR</b><br><br>")
+// var USDollar =10;
+// var SaudiRiyals = 25;
+// var PKR = USDollar * 307.79 + SaudiRiyals * 82.05305;
+//  document.write("Total Currency in PKR: "+PKR);
+
+//PROGRAM NO 10
+
+// var x = 2;
+// var result = x+5*10/2;
+// document.write("Result: "+result);
+
+//PROGRAM NO 11
+// document.write("<h1><b>Age Calculator</b></h1><br><br>");
+// var CurrentYear = 2016;
+// var BirthYear = 1992;
+// var age = CurrentYear-BirthYear;
+// document.write("Your Age is: "+age);
+
+//PROGRAM NO 12
+// document.write("<h1><b>The Geometrizer</b></h1><br>")
+//   var radius = 20;
+//   document.write("Radius of a circle: " + radius +"<br>");
+//  var circumference = 2 *3.142*radius;
+//  document.write("The circumference is: " + circumference +"<br>");
+//  var area = 3.142*radius*radius;
+//  document.write("The aera is: "+area);
+
+//PROGRAM NO 13 
+// document.write("<h1><b>The LifeTime Supply Calculator</b></h1><br>");
+// var Favoritesnack = "chocolate chip";
+// document.write("Favorite Snack: "+Favoritesnack+"<br>")
+// var currentAge = 19;
+// document.write("Current Age: "+currentAge+"<br>")
+// var EstMaxAge = 65;
+// document.write("Estimated Maximum Age: "+EstMaxAge+"<br>")
+// var snacksPerDay = 3;
+// document.write("Amount Of Snacks per day: "+snacksPerDay+"<br>")
+
+// var yearsRemaining = EstMaxAge - currentAge;
+// var snacksPerYear = snacksPerDay * 365;
+// var totalSnacksRequired = yearsRemaining * snacksPerYear;
+// document.write("You will need "+totalSnacksRequired+" chocolate chip to last you until the ripe old age of "+EstMaxAge);
